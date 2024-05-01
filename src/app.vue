0c8fdd40cbdf8cab5dd4a847bead8cd1109da401
Brays83 <102981505+Brays83@users.noreply.github.com>
2024-04-30 21:07:23 -0500

    feat(proyects):add component home, task and toolbar

---
+++ src/app.vue
@@ -0,0 +1,29 @@
+<script>
+import ToolbarComponent from "./public/component/toolbarStudent.component.vue";
+export default {
+  name: 'app',
+  title: 'DevSoft TeamSync',
+  components: {ToolbarComponent},
+  data(){
+    return{
+
+    }
+  },
+  methods(){
+
+  }
+}
+</script>
+
+<template>
+  <ToolbarComponent></ToolbarComponent>
+  <router-view></router-view>
+
+
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/proyect/pages/tasks-proyect.component.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "tasks-proyect.component"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
