f80786658165cb6c9833abb29bfc103ef94bec87
Brays83 <102981505+Brays83@users.noreply.github.com>
2024-05-10 02:15:10 -0500

    feat(Members): Add button Invite and delete integrate

--- src/proyect/pages/members-proyect.component.vue
+++ src/proyect/pages/members-proyect.component.vue
@@ -1,6 +1,8 @@
 <script>
 import {ProyectApiService} from "../services/proyect-api.service.js";
 
+
+
 export default {
   name: "members-proyect.component",
   title: "toolBarStudent",
@@ -41,29 +43,49 @@ export default {
     <div class="grid overflow-auto xl: h-13rem " >
       <div class="xl:col-4 md:col-6 sm:col-12"  v-for="integrate in integratesOfProyect" :key="integrate.id" >
         <div class="card1">
-          <img src="https://support-leagueoflegends.riotgames.com/hc/article_attachments/7396100117907/star-guardian-kaisa-icon.png">
-          <h3>{{integrate.name}}</h3>
+          <div>
+            <img src="https://support-leagueoflegends.riotgames.com/hc/article_attachments/7396100117907/star-guardian-kaisa-icon.png">
+            <h3>{{integrate.name}}</h3>
+          </div>
+          <pv-button text>
+            <i class="pi pi-trash"  id="buttonMembers"></i>
+          </pv-button>
         </div>
       </div>
     </div>
   </div>
 
+  <div class="flex p-2 m-3">
+    <pv-button label="Invite" style="background-color: #058339;"/>
+  </div>
+
+
+
 
 </template>
 
 <style scoped>
+#buttonMembers {
+  font-size:2rem;
+  color:black;
+
+ }
 .card1{
   display:flex;
+  justify-content: space-between;
 
 
   border-radius:16px;
 
-  align-items:flex-start;
+
   padding:5px;
   background-color: #C2CEFF;
   margin: 10px;
+  >div{
+    display:flex;
+  }
 
-  >img{
+  >div>img{
     height:51px;
     width:51px;
     border-radius:50%;
