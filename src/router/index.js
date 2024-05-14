8f5ed31f6873d2021f2b059b27910c1f2146f069
Brays83 <102981505+Brays83@users.noreply.github.com>
2024-05-13 21:14:53 -0500

    feat(router): Add routers childrens of project

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,24 @@ import HomeProyectComponent from "../proyect/pages/home-proyect.component.vue";
 import TasksProyectComponent from "../proyect/pages/tasks-proyect.component.vue";
 import ExpertsComponent from "../expert/pages/experts.component.vue";
 import MembersProyectComponent from "../proyect/pages/members-proyect.component.vue";
+import HomeComponent from "../public/pages/home.component.vue";
+import LayoutComponent from "../proyect/pages/layout.component.vue";
 let  id;
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {path:`/id/home`,component: HomeProyectComponent, meta:{title:'Home'}},
-        {path:`/id/tasks`,component:TasksProyectComponent,meta:{title:'Tasks'}},
-        {path:`/id/experts`,component:ExpertsComponent,meta:{title:'Experts'}},
-        {path:'/id/members',component:MembersProyectComponent}
+
+        {path:`/project/:id`,component:LayoutComponent,children:[
+                {path: "", redirect:"home"},
+                {path:`home`,component: HomeProyectComponent, meta:{title:'HomeProject'} ,name:'myProject'},
+                {path:`tasks`,component:TasksProyectComponent,meta:{title:'Tasks'}},
+                {path:`experts`,component:ExpertsComponent,meta:{title:'Experts'}},
+                {path:'members',component:MembersProyectComponent, meta:{title:'Members'}}
+            ]},
+        {path: "", redirect: "/home" },
+        {path:'/home',component:HomeComponent,meta:{title:'HomePublic'}}
+
+
 
 
     ]
